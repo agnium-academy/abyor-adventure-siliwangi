54b2cddad15648c8e2579ead304939e45d5ae25b
trioandianto <trio.andianto@basajans.com>
2016-02-03 11:45:45 +0700

    Menambahkan Model Student sama Repository

---
+++ Students/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Students.Models
+{
+    public class Student
+    {
+        [Required]
+        public int NIM { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Alamat { get; set; }
+        [Required]
+        public int umur { get; set; }
+    }
+}
